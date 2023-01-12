d33e643c964f329c320653caf15d81baa2a8f5d6
Chrsteffen <Chrsteffen1@gmail.com>
2023-01-11 19:34:06 -0500

    add paper and scissors buttons

--- src/Components/ChoiceCard.jsx
+++ src/Components/ChoiceCard.jsx
@@ -3,8 +3,9 @@ import Button from 'react-bootstrap/Button';
 const ChoiceCard = () => {
   return (
     <div>
-    
       <Button>Rock</Button>
+      <Button>Paper</Button>
+      <Button>Scissors</Button>
     </div>
   );
 }
