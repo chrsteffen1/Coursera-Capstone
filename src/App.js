dd8ba291f85c47c16dbd0f5770f63812bd6852da
Chrsteffen <Chrsteffen1@gmail.com>
2023-01-11 19:32:27 -0500

    refactor to rock paper scissors game

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HomePage</h1>
+      <h1>Little Lemon</h1>
     </div>
   );
 }
---
+++ src/Components/ChoiceCard.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+const ChoiceCard = () => {
+  return (
+    <div>
+    
+      <Button>Rock</Button>
+    </div>
+  );
+}
+
+export default ChoiceCard;
