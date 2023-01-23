e6b7fb9fcdf588e6975be6c97b05a2eac1083e95
Chrsteffen <Chrsteffen1@gmail.com>
2023-01-23 10:29:07 -0500

    set comp choice

--- src/Components/ChoiceCard.jsx
+++ src/Components/ChoiceCard.jsx
@@ -3,17 +3,33 @@ import { useState } from 'react';
 
 const ChoiceCard = () => {
   const [choice, setChoice] = useState('Start')
+  const [color, setColor] = useState("outline-dark")
+  const [compChoice, setCompChoice] = useState('')
 
   const handleOnClick = evt => {
     setChoice(evt.target.value)
+    
   }
+  const getComputerChoice= () => {
+    let randomNumber= Math.floor(Math.random() * 3)
+      if(randomNumber === 0){
+        setCompChoice('Rock')
+      }
+      if(randomNumber === 1){
+        setCompChoice('Paper')
+      }
+      if(randomNumber === 2){
+        setCompChoice('Scissors')
+      }
+ }
 
   return (
     <div>
-      <Button variant='primary' onClick={handleOnClick} value='Rock'>Rock</Button>
-      <Button onClick={handleOnClick} value='Paper'>Paper</Button>
-      <Button onClick={handleOnClick} value='Scissors'>Scissors</Button>
-      {choice}
+      <Button variant={color} onClick={handleOnClick} value='Rock'>Rock</Button>
+      <Button variant={color} onClick={handleOnClick} value='Paper'>Paper</Button>
+      <Button variant={color} onClick={handleOnClick} value='Scissors'>Scissors</Button>
+      <Button onClick={getComputerChoice}>Play game</Button>
+      {compChoice}
     </div>
   );
 }
